3225701b70815a6b19e710ad1cdedf8858ee23bb
Paweł Kukliński <paffcio314@gmail.com>
2019-09-17 19:27:18 +0200

    Finished otodom crawler functionality (runs indefinitely)

--- housy/spiders/otodom_spider.py
+++ housy/spiders/otodom_spider.py
@@ -1,6 +1,7 @@
 import scrapy
 from unidecode import unidecode
 from parsedatetime import Calendar
+import os
 
 from housy.requirements.requirements import req
 from housy.requirements.otodom import OtodomRequirements
@@ -38,16 +39,34 @@ class OtodomSpider(scrapy.Spider):
                 tmp_list.append(str_bit)
             return ' '.join(tmp_list)
 
-        li = response.xpath("//li/text()")
-        li_text = get_processed_text(li)
-        p = response.xpath("//p/text()")
-        p_text = get_processed_text(p)
-        text = ' '.join([li_text, p_text])
+        def calculate_intersection(text_to_look_through, words_to_search):
+            number_of_words_to_search = len(words_to_search)
+            number_of_words_present = sum([1 for word in words_to_search if word in text_to_look_through])
+            return number_of_words_present/number_of_words_to_search
+
         # Extract date of offer submission
+        # We don't want an offer which is too old
         unprocessed_date = response.xpath("//div[@class='css-lh1bxu']").get()
         (_, _, unprocessed_date) = unprocessed_date.partition(':')
         (processed_date, _, _) = unprocessed_date.partition('<')
         cal = Calendar()
         offer_submission_date = cal.parse(processed_date)
         otodom_req = OtodomRequirements(req)
-        # TODO crawler should return file with urls which meet requirements
+        required_date = cal.parse(' '.join([str(otodom_req.number_of_days), 'days ago']))
+        if offer_submission_date < required_date:
+            return
+
+        li = response.xpath("//li/text()")
+        li_text = get_processed_text(li)
+        p = response.xpath("//p/text()")
+        p_text = get_processed_text(p)
+        text = ' '.join([li_text, p_text])
+        threshold = 0.99  # TODO add threshold to requirements
+        if calculate_intersection(text, otodom_req.tags) >= threshold:
+            script_directory = os.path.dirname(os.path.realpath(__file__))
+            tmp_path = os.path.realpath(script_directory).split('/')
+            tmp_path = '/'.join(tmp_path[:(len(tmp_path)-1)])
+            path = '/'.join([tmp_path, 'scrapy-data/urls.txt'])
+            with open(file=path, mode='a') as f:
+                f.write(response.url)
+                f.write('\n')
